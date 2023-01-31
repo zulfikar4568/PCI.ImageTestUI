92a34c5d4d933ee6bd8e2372b4711ffd2f811dca
zulfikar4568 <isnaen70@gmail.com>
2023-01-31 12:51:27 +0700

    feat: itext pdf

---
+++ PCI.ImageTestUI/Util/PdfUtil.cs
@@ -0,0 +1,33 @@
+﻿using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using System;
+using System.Collections.Generic;
+using iText.IO.Image;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iText.Layout.Element;
+
+namespace PCI.ImageTestUI.Util
+{
+    public class PdfUtil
+    {
+        protected void MergeImageToPdf(string sourceFile, Image[] images)
+        {
+            Image image = images[0];
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(sourceFile));
+            Document doc = new Document(pdfDoc, new PageSize(image.GetImageWidth(), image.GetImageHeight()));
+
+            for (int i = 0; i < images.Length; i++)
+            {
+                image = images[i];
+                pdfDoc.AddNewPage(new PageSize(image.GetImageWidth(), image.GetImageHeight()));
+                image.SetFixedPosition(i + 1, 0, 0);
+                doc.Add(image);
+            }
+
+            doc.Close();
+        }
+    }
+}
